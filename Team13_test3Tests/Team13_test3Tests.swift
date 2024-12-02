63fc7a341df1e4b01121baa9e044644d6854ec94
shashvat <singhshashvat4@gamil.com>
2024-12-02 15:42:12 +0530

    Initial Commit

---
+++ Team13_test3Tests/Team13_test3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Team13_test3Tests.swift
+//  Team13_test3Tests
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import Testing
+@testable import Team13_test3
+
+struct Team13_test3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Team13_test3UITests/Team13_test3UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Team13_test3UITestsLaunchTests.swift
+//  Team13_test3UITests
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import XCTest
+
+final class Team13_test3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
